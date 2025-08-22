270996eba7bd62c9acf821b86179e4cb8e89a44b
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-08-22 09:13:22 +0900

    style: terraform fmt

--- terraform/2_folders/main.tf
+++ terraform/2_folders/main.tf
@@ -1,13 +1,13 @@
 # "Production"フォルダ
 resource "google_folder" "production" {
-  display_name = "Production"
-  parent       = "organizations/${var.organization_id}"
-  deletion_protection=false
+  display_name        = "Production"
+  parent              = "organizations/${var.organization_id}"
+  deletion_protection = false
 }
 
 # "Development"フォルダ
 resource "google_folder" "development" {
-  display_name = "Development"
-  parent       = "organizations/${var.organization_id}"
-  deletion_protection=false
+  display_name        = "Development"
+  parent              = "organizations/${var.organization_id}"
+  deletion_protection = false
 }
