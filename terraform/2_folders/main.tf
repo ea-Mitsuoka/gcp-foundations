e6b3ec705382913ff182f49b36930919fd4409c9
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-08-23 19:11:24 +0900

    chore: upgrade hashicorp/google provider version

--- terraform/2_folders/main.tf
+++ terraform/2_folders/main.tf
@@ -2,6 +2,8 @@
 resource "google_folder" "production" {
   display_name        = "Production"
   parent              = "organizations/${var.organization_id}"
+
+  # destroy（削除）を許可するフラグ
   deletion_protection = false
 }
 
@@ -9,5 +11,7 @@ resource "google_folder" "production" {
 resource "google_folder" "development" {
   display_name        = "Development"
   parent              = "organizations/${var.organization_id}"
+
+  # destroy（削除）を許可するフラグ
   deletion_protection = false
 }
