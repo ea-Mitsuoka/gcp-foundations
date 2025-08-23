50c09bffb22e412d4b51ab0498de0c3ca4115d49
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-08-23 23:46:29 +0900

    fix: update function version reference

--- terraform/3_projects/example_project/main.tf
+++ terraform/3_projects/example_project/main.tf
@@ -1,5 +1,5 @@
 module "string_utils" {
-  source            = "git::https://gitea.mtskykhd.tokyo/admin/terraform-modules.git//string_utils"
+  source            = "git::https://gitea.mtskykhd.tokyo/admin/terraform-modules.git//string_utils?ref=1f2baf4468"
   organization_name = var.organization_name
   env               = var.labels.env
   app               = var.labels.app
