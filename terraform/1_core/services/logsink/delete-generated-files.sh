5cb36e6d9a50e8d49b16e9af0623e1bbbb04dbe0
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-09-11 15:03:00 +0900

    chore: make minor changes

--- terraform/1_core/services/logsink/delete-generated-files.sh
+++ terraform/1_core/services/logsink/delete-generated-files.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -rf ./data.tf
 rm -rf ./sinks.tf
 rm -rf ./destinations.tf
 rm -rf ./iam.tf
 
-echo "Pythonスクリプトによるtfファイル生成をやり直すために data.tf, sinks.tf, destinations.tf, iam.tf を 削除しました"
+echo "Pythonスクリプトによるtfファイル生成をやり直すために sinks.tf, destinations.tf, iam.tf を 削除しました"
