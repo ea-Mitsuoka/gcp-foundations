7c049847daa3799031a687ad656d483642bd2e1f
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-09-18 17:15:37 +0900

    chore: make minor changes

--- terraform/1_core/services/logsink/iam/data.tf
+++ terraform/1_core/services/logsink/iam/data.tf
@@ -2,6 +2,6 @@ data "terraform_remote_state" "project" {
   backend = "gcs"
   config = {
     bucket = var.gcs_backend_bucket
-    prefix = "core/base/monitoring"
+    prefix = "core/base/logsink"
   }
 }
