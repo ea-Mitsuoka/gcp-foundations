eec433d8f0a275237c4a50a7a9a9719da599d00f
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2026-03-25 18:50:30 +0900

    fix(terraform): correct referenced commit ID of external module

--- terraform/1_core/base/logsink/main.tf
+++ terraform/1_core/base/logsink/main.tf
@@ -11,7 +11,7 @@ locals {
 
 # 外部モジュール（string_utils）の呼び出し
 module "string_utils" {
-  source = "git::https://github.com/ea-Mitsuoka/terraform-modules.git//string_utils?ref=610dae0"
+  source = "git::https://github.com/ea-Mitsuoka/terraform-modules.git//string_utils?ref=535a37e"
   # 修正点: データソースからではなく、変数を直接使用
   organization_name = local.sanitized_domain
   env               = var.labels.env
