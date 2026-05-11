fb1862a83bd91a93cf8e284d25a2fbaa40d9b7e8
ea-Mitsuoka <yukihide.mitsuoka@e-agency.co.jp>
2026-05-12 06:17:10 +0900

    chore(terraform): remove auto-generated output files for subnets and tags

--- terraform/1_core/base/vpc-host/auto_subnets.tf
+++
@@ -1,9 +0,0 @@
-# Auto-generated file. Do not edit manually.
-
-output "shared_vpc_subnet_ids" {
-  description = "Map of shared VPC subnet names to their resource IDs."
-  value = {
-
-  }
-}
-
--- terraform/2_organization/auto_tags.tf
+++
@@ -1,8 +0,0 @@
-# Auto-generated file. Do not edit manually.
-
-output "tag_value_ids" {
-  description = "Map of organization tag key/value pairs to their resource IDs."
-  value = {
-  }
-}
-
